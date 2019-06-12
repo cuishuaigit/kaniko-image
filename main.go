50707e14a217f7b141c4a8f2d951ad629daba1a4
cuishuai <225cuishuai@gmail.com>
2019-06-12 13:42:15 +0800

    use pod replace deployemnt kaniko

--- main.go
+++ main.go
@@ -6,11 +6,9 @@ import (
 	"path/filepath"
 	"strings"
 
-	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/typed/apps/v1"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
@@ -21,7 +19,7 @@ var (
 	repo       string
 	project    string
 	registry   string
-	seq         []string
+	seq        []string
 )
 
 func main() {
@@ -32,7 +30,7 @@ func main() {
 	}
 	flag.StringVar(&repo, "repo", "", "provide a repo of your project")
 	flag.StringVar(&namespace, "n", "default", "namespace")
-	flag.StringVar(&project, "p", "", "your projrct name")
+	flag.StringVar(&project, "p", "", "your project name")
 	flag.StringVar(&registry, "r", "", "your registry name")
 	flag.Parse()
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
@@ -43,121 +41,84 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	deploymentClient := clientset.AppsV1().Deployments(namespace)
-	if getDeployment(deploymentClient) == project {
-		deleteDeployment(deploymentClient)
-	}
-	createDeployment(deploymentClient)
-}
-
-func int32Ptr(i int32) *int32 { return &i }
-// CreateDeployment
-func createDeployment(deploymentClient v1.DeploymentInterface) {
 	canSplit := func(s rune) bool { return s == '/' }
 	seq := strings.FieldsFunc(registry, canSplit)
-	deployment := &appsv1.Deployment{
+	pod := &apiv1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      project,
 			Namespace: namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: int32Ptr(1),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "kaniko",
-				},
+			Labels: map[string]string{
+				"app": "kaniko",
 			},
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "kaniko",
-					},
-				},
-				Spec: apiv1.PodSpec{
-					InitContainers: []apiv1.Container{
+		},
+		Spec: apiv1.PodSpec{
+			InitContainers: []apiv1.Container{
+				{
+					Name:            "init-repo",
+					Image:           "alpine/git",
+					ImagePullPolicy: apiv1.PullPolicy(apiv1.PullIfNotPresent),
+					Command:         []string{"git", "clone", repo},
+					VolumeMounts: []apiv1.VolumeMount{
 						{
-							Name:            "init-repo",
-							Image:           "alpine/git",
-							ImagePullPolicy: apiv1.PullPolicy(apiv1.PullIfNotPresent),
-							Command:         []string{"git", "clone", repo},
-
-							VolumeMounts: []apiv1.VolumeMount{
-								{
-									Name:      "workdir",
-									MountPath: "/git",
-								},
-							},
-						},
-					},
-					Containers: []apiv1.Container{
-						{
-							Name:            "kaniko",
-							Image:           "gcr.io/kaniko-project/executor:debug",
-							ImagePullPolicy: apiv1.PullPolicy(apiv1.PullIfNotPresent),
-							Args:            []string{"--dockerfile=/workspace/" + project + "/Dockerfile", "--context=dir://workspace/" + project, "--destination=" + registry,"--cache=true","--cache-repo="+seq[0]+"/cache","--cleanup"},
-							VolumeMounts: []apiv1.VolumeMount{
-								{
-									Name:      "workdir",
-									MountPath: "/workspace",
-								},
-								{
-									Name:      "kaniko-secret",
-									MountPath: "/kaniko/.docker/",
-								},
-							},
-						},
-					},
-					NodeSelector: map[string]string{
-						"kaniko":"enabled",
-					} ,
-					Tolerations: []apiv1.Toleration{
-						{
-							Key: "kaniko",
-							Operator: apiv1.TolerationOperator(apiv1.TolerationOpEqual),
-							Value: "enabled",
-							Effect: apiv1.TaintEffect(apiv1.TaintEffectNoSchedule),
-							},
-					},
-					Volumes: []apiv1.Volume{
-						apiv1.Volume{
-							Name: "kaniko-secret",
-							VolumeSource: apiv1.VolumeSource{
-								Secret: &apiv1.SecretVolumeSource{
-									SecretName: "kaniko-secret",
-								},
-							},
-						},
-						apiv1.Volume{
-							Name: "workdir",
+							Name:      "workdir",
+							MountPath: "/git",
 						},
 					},
 				},
 			},
+			Containers: []apiv1.Container{
+				{
+					Name:            "kaniko",
+					Image:           "gcr.io/kaniko-project/executor:debug",
+					ImagePullPolicy: apiv1.PullPolicy(apiv1.PullIfNotPresent),
+					Args:            []string{"--dockerfile=/workspace/" + project + "/Dockerfile", "--context=dir://workspace/" + project, "--destination=" + registry, "--cache=true", "--cache-repo=" + seq[0] + "/cache", "--cleanup"},
+					VolumeMounts: []apiv1.VolumeMount{
+						{
+							Name:      "workdir",
+							MountPath: "/workspace",
+						},
+						{
+							Name:      "kaniko-secret",
+							MountPath: "/kaniko/.docker/",
+						},
+					},
+				},
+			},
+			NodeSelector: map[string]string{
+				"kaniko": "enabled",
+			},
+			Tolerations: []apiv1.Toleration{
+				{
+					Key:      "kaniko",
+					Operator: apiv1.TolerationOperator(apiv1.TolerationOpEqual),
+					Value:    "enabled",
+					Effect:   apiv1.TaintEffect(apiv1.TaintEffectNoSchedule),
+				},
+			},
+			RestartPolicy: apiv1.RestartPolicy(apiv1.RestartPolicyNever),
+			Volumes: []apiv1.Volume{
+				apiv1.Volume{
+					Name: "kaniko-secret",
+					VolumeSource: apiv1.VolumeSource{
+						Secret: &apiv1.SecretVolumeSource{
+							SecretName: "kaniko-secret",
+						},
+					},
+				},
+				apiv1.Volume{
+					Name: "workdir",
+				},
+			},
 		},
 	}
-	result, err := deploymentClient.Create(deployment)
+
+	// Deletepods
+	err = clientset.CoreV1().Pods(namespace).Delete(project, &metav1.DeleteOptions{})
+	if err != nil {
+		fmt.Printf("delete pod err :%v", err)
+	}
+	_, err = clientset.CoreV1().Pods(namespace).Create(pod)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Create deployment %q.\n", result.GetObjectMeta().GetName())
-}
-
-// DeleteDeployment
-func deleteDeployment(deploymentClient v1.DeploymentInterface) {
-	deletePolicy := metav1.DeletePropagationBackground
-	if err := deploymentClient.Delete(project, &metav1.DeleteOptions{
-		PropagationPolicy: &deletePolicy,
-	}); err != nil {
-		panic(err)
-	}
-}
-
-// Getdeployment
-func getDeployment(deploymentClient v1.DeploymentInterface) string {
-	deployment, err := deploymentClient.Get(project, metav1.GetOptions{})
-	if err != nil {
-		fmt.Printf("%s\n", err)
-	}
-	// fmt.Printf("%s\n", deployment.Name)
-	return deployment.Name
 }
